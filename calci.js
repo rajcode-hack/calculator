8c997e89f5c8e135643eea445878b50d19e4d445
rajcode-hack <116270212+rajcode-hack@users.noreply.github.com>
2022-11-07 16:37:50 +0530

    analog calculator

---
+++ calci.js
@@ -0,0 +1,23 @@
+// Todo:Make M+ M- and Mc functional
+let string = "";
+let buttons = document.querySelectorAll(".button");
+console.log(Array.from(buttons));
+Array.from(buttons).forEach((button) => {
+    button.addEventListener("click", (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+        else if (e.target.innerHTML == 'C') {
+            string = " ";
+            document.querySelector('input').value = string;
+        }
+        else {
+            console.log(e.target)
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+
+        }
+
+    })
+})
